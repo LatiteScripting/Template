091a207de03af539c18e1a4678f62ded632af268
Plextora <71889427+Plextora@users.noreply.github.com>
2023-08-18 18:16:01 -0400

    Updated definitions

--- definitions/lib/network.d.ts
+++ definitions/lib/network.d.ts
@@ -1,60 +1,56 @@
 declare namespace include {
-    interface HttpResponse {
-      /**
-       * The body, if the status code is 200 (OK)
-       */
-      body: Uint8Array;
-      statusCode: number;
+    export interface HttpResponse {
+        /**
+         * The body, if the status code is 200 (OK)
+         */ 
+        body: Buffer,
+        statusCode: number,
 
-      /**
-       * A WinRT error. Undefined if no error
-       */
-      error: string;
+        /**
+         * The http client error message
+         */
+        error?: string,
     }
 
     interface GetData {
-      contentType?: string;
-      content?: Uint8Array;
+        contentType?: string,
+        content?: Buffer,
     }
 
     interface PostData {
-      contentType?: string;
-      content: Uint8Array;
+        contentType?: string,
+        content: Buffer,
     }
 
     interface Network {
-      /**
-       * Sends a basic GET request to a URL.
-       * @param url The URL to send the request.
-       * @param data
-       */
-      getSync(url: string, data?: GetData): HttpResponse;
+        /**
+         * Sends a basic GET request to a URL.
+         * @param url The URL to send the request.
+         * @param data 
+         */
+        getSync(url: string, data?: GetData): HttpResponse;
 
-      /**
-       * Sends an async GET request to a URL.
-       * @param url The URL to send the request.
-       * @param data
-       * @param callback
-       */
-      get(
-        url: string,
-        data: GetData,
-        callback: (err: number, resp: HttpResponse) => void
-      ): void;
+        /**
+         * Sends an async GET request to a URL.
+         * @param url The URL to send the request.
+         * @param data 
+         * @param callback 
+         */
+        get(url: string, data: GetData, callback: (resp: HttpResponse) => void): void;
+        
+        /**
+         * Sends a basic POST request to a URL.
+         * @param url The URL to send a POST request.
+         * @param data 
+         */
+        postSync(url: string, data?: PostData): HttpResponse;
 
-      /**
-       * Sends a basic POST request to a URL.
-       * @param url The URL to send a POST request.
-       * @param data
-       */
-      postSync(url: string, data?: PostData): HttpResponse;
-
-      /**
-       * Sends an async POST request to a URL.
-       * @param url The URL to send the request.
-       * @param data
-       * @param callback
-       */
-      //post(url: string, data: GetData, callback: (err: number, resp: HttpResponse) => void): void;
+        /**
+         * Sends an async POST request to a URL.
+         * @param url The URL to send the request.
+         * @param data 
+         * @param callback 
+         */
+        //post(url: string, data: GetData, callback: (err: number, resp: HttpResponse) => void): void;
     }
 }
--- definitions/util/util.d.ts
+++ definitions/util/util.d.ts
@@ -1,18 +1,14 @@
 interface IUtil {
-<<<<<<< HEAD
-    bufferToString(buf: Uint8Array): string;
-=======
     /**
      * Converts a buffer into a UTF-8 string.
      * @param buf 
      */
-    bufferToString(buf: Uint8Array): string;
+    bufferToString(buf: Buffer): string;
     /**
      * Converts a string to a UTF-8 buffer.
      * @param str 
      */
->>>>>>> 4450da648b444dd69f9648e8728a6cb7702074b8
-    stringToBuffer(str: string): Uint8Array;
+    stringToBuffer(str: string): Buffer;
 }
 
 declare var util: IUtil;
